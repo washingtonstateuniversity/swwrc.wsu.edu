548a5d260446af7042d9a402f77c88db6216ffaa
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2014-11-03 13:43:02 -0800

    Change the image paths for the homepage bg video

--- parts/before-binder.php
+++ parts/before-binder.php
@@ -1,13 +1,13 @@
 <div class="videobg">
 	<div class="banner-container">
 		<div class="logo-container">
-	<img src="/wp-content/uploads/sites/5/2014/10/SWWRC-logo-vert-reverse.png" alt="State of Washington Water Research Center" class="home-logo" />
+	<img src="/wp-content/uploads/sites/354/2014/11/SWWRC-logo-vert-reverse1.png" alt="State of Washington Water Research Center" class="home-logo" />
 </div>
 <h1>Water is an essential resource for the economic, social, and cultural well-being of the state of Washington. </h1>
 </div>
 </div>
 <nav class="main-menu">
-	<img id="logo" src="/wp-content/uploads/sites/5/2014/10/SWWRC-logo-vert-rgb.png" alt="State of Washington Water Research"/><?php
+	<img id="logo" src="/wp-content/uploads/sites/354/2014/11/SWWRC-logo-vert-rgb.png" alt="State of Washington Water Research"/><?php
 	$spine_site_args = array(
 		'theme_location'  => 'site',
 		'menu'            => 'site',
