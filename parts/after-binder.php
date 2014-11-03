a5fc4608c0ab64848cce93130ca82244462f4564
Charles J Dillon <charlesjdillon@ucadmins-MacBook-Pro.local>
2014-11-03 13:08:43 -0800

    Initial commit

---
+++ parts/after-binder.php
@@ -0,0 +1,25 @@
+<footer>
+		<address itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+			<meta itemprop="name" content="<?php echo esc_attr( spine_get_option( 'contact_name' ) ); ?>">
+	<meta itemprop="department" content="<?php echo esc_attr( spine_get_option( 'contact_department' ) ); ?>">
+		<?php echo esc_attr( spine_get_option( 'contact_department' ) ); ?><br />
+		<meta itemprop="streetAddress" content="<?php echo esc_attr( spine_get_option( 'contact_streetAddress' ) ); ?>">
+		<?php echo esc_attr( spine_get_option( 'contact_streetAddress' ) ); ?><br />
+		<meta itemprop="addressLocality" content="<?php echo esc_attr( spine_get_option( 'contact_addressLocality' ) ); ?>">
+		<?php echo esc_attr( spine_get_option( 'contact_addressLocality' ) ); ?><br />
+		<meta itemprop="postalCode" content="<?php echo esc_attr( spine_get_option( 'contact_postalCode' ) ); ?>">
+
+	<meta itemprop="telephone" content="<?php echo esc_attr( spine_get_option( 'contact_telephone' ) ); ?>">
+	<?php echo esc_attr( spine_get_option( 'contact_telephone' ) ); ?><br />
+	<meta itemprop="email" content="<?php echo esc_attr( spine_get_option( 'contact_email' ) ); ?>">
+	<a href="mailto:<?php echo esc_attr( spine_get_option( 'contact_email' ) ); ?><"><?php echo esc_attr( spine_get_option( 'contact_email' ) ); ?></a>
+	
+	<?php
+		$contact_point = spine_get_option( 'contact_ContactPoint' );
+		if ( ! empty( $contact_point ) ) {
+			?><meta itemprop="ContactPoint" title="<?php echo esc_attr( spine_get_option( 'contact_ContactPointTitle' ) ); ?>" content="<?php echo esc_attr( $contact_point ); ?>">
+			<?php
+		}
+	?> 
+</address>
+</footer>
---
+++ parts/before-binder.php
@@ -0,0 +1,8 @@
+<div class="videobg">
+	<div class="banner-container">
+		<div class="logo-container">
+	<img src="/wp-content/uploads/sites/5/2014/10/SWWRC-logo-vert-reverse.png" alt="State of Washington Water Research Center" class="home-logo" />
+</div>
+<h1>Water is an essential resource for the economic, social, and cultural well-being of the state of Washington. </h1>
+</div>
+</div>
