0ec84d408d87d92512faf81b22f3de4d085bdcff
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2014-11-05 10:40:04 -0800

    Add is front page to homepage splash

--- parts/before-binder.php
+++ parts/before-binder.php
@@ -1,11 +1,14 @@
+ <?php if( is_front_page() ) { ?>
 <div class="videobg">
 	<div class="banner-container">
 		<div class="logo-container">
 	<img src="/wp-content/uploads/sites/354/2014/11/SWWRC-logo-vert-reverse1.png" alt="State of Washington Water Research Center" class="home-logo" />
 </div>
+
 <h1>Water is an essential resource for the economic, social, and cultural well-being of the state of Washington. </h1>
 </div>
 </div>
+<?php } ?>
 <nav class="main-menu">
 	<img id="logo" src="/wp-content/uploads/sites/354/2014/11/SWWRC-logo-vert-rgb.png" alt="State of Washington Water Research"/><?php
 	$spine_site_args = array(
