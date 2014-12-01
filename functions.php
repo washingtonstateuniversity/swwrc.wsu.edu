6cbbda376c5de4ac2ce0640ea979dc6e4472ec65
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2014-12-01 13:47:57 -0800

    Set pre-req to jquery

--- functions.php
+++ functions.php
@@ -8,5 +8,5 @@ add_action( 'wp_enqueue_scripts', 'swwrc_child_enqueue_scripts', 11 );
  * Enqueue custom scripting in child theme.
  */
 function swwrc_child_enqueue_scripts() {
-	wp_enqueue_script( 'swwrc-custom', get_stylesheet_directory_uri() . '/custom.js', array( 'swwrc-setup-nav' ), spine_get_script_version(), true );
+	wp_enqueue_script( 'swwrc-custom', get_stylesheet_directory_uri() . '/custom.js', array( 'jquery' ), spine_get_script_version(), true );
 }
