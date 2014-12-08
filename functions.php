bd6bb4d2f65f38528cb4873a418a8c9acf75bb5c
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2014-12-08 11:51:41 -0800

    Look for p tag, if false then add p tag

--- functions.php
+++ functions.php
@@ -14,3 +14,19 @@ function swwrc_child_enqueue_scripts() {
 	wp_enqueue_script( 'swwrc-videobg', get_stylesheet_directory_uri() . '/js/jQuery.videobg.js', array( 'jquery' ), spine_get_script_version(), true );
 	wp_enqueue_script( 'swwrc-custom', get_stylesheet_directory_uri() . '/custom.js', array( 'jquery' ), spine_get_script_version(), true );
 }
+add_filter( 'get_the_excerpt', 'swwrc_trim_excerpt', 6 );
+/**
+ * Provide a custom trimmed excerpt forced for SWWRC.
+ * Custom excerpt to reject custom DOM while Spine Parent theme is addressed
+ *
+ * @param string $text The raw excerpt.
+ *
+ * @return string The modified excerpt.
+ */
+function swwrc_trim_excerpt( $text ) {
+	$raw_excerpt = $text;
+	if ( '' !== $text && strpos($text, "<p>") !== false) {
+		$text = "<p>".$text."</p>";
+	}
+	return $text;
+}
