4318c11366d5ea5e741fae550869361494611954
Phil Cable <pcable@wsu.edu>
2017-08-16 10:09:34 -0700

    Extend `get_terms` with a `post_type` parameter

--- functions.php
+++ functions.php
@@ -75,3 +75,37 @@ function swwrc_filter_syndicate_host_data( $subset, $post ) {
 
 	return $subset;
 }
+
+add_filter( 'terms_clauses', 'swwrc_post_type_terms_clauses', 10, 3 );
+/**
+ * Extend `get_terms` with a `post_type` parameter.
+ *
+ * @since 0.5.0
+ *
+ * @param string $clauses
+ * @param string $taxonomy
+ * @param array  $args
+  *
+ * @return string
+ */
+function swwrc_post_type_terms_clauses( $clauses, $taxonomy, $args ) {
+	if ( isset( $args['post_type'] ) && ! empty( $args['post_type'] ) ) {
+		global $wpdb;
+		$post_types = array();
+
+		if ( is_array( $args['post_type'] ) ) {
+			foreach ( $args['post_type'] as $post_type ) {
+				$post_types[] = $post_type;
+			}
+		} else {
+			$post_types[] = $args['post_type'];
+		}
+
+		if ( ! empty( $post_types ) ) {
+			$clauses['join'] .= " INNER JOIN $wpdb->term_relationships AS r ON r.term_taxonomy_id = tt.term_taxonomy_id INNER JOIN $wpdb->posts AS p ON p.ID = r.object_id";
+			$clauses['where'] .= $wpdb->prepare( ' AND p.post_type IN (%s) GROUP BY t.term_id', implode( ',', $post_types ) );
+		}
+	}
+
+	return $clauses;
+}
