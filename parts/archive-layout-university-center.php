200b45c102d46fd177087136f68fd02236ab1d36
Phil Cable <pcable@wsu.edu>
2017-09-22 13:42:31 -0700

    Fix up the UC post type archive template
    This uses the appropriate function for displaying the archive title
    and removes the call to output the filter markup, as it is no longer
    needed.

--- parts/archive-layout-university-center.php
+++ parts/archive-layout-university-center.php
@@ -1,12 +1,8 @@
 <section class="row side-right gutter pad-ends">
 
-	<?php $post_type = ( get_queried_object() ) ? get_queried_object() : false; ?>
-
-	<h1><?php echo esc_html( $post_type->label ); ?></h1>
+	<h1><?php post_type_archive_title(); ?></h1>
 
 	<div class="column one">
-
-		<?php SWWRC\University_Center_Objects\display_tag_filters( $post_type->name ); ?>
 
 		<div>
 
